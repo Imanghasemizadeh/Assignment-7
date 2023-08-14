df22d9ae38e2ce30a9899be4383c17d84a80612a
Imanghasemizadeh <140438269+Imanghasemizadeh@users.noreply.github.com>
2023-08-14 20:37:58 +0330

    Add files via upload

---
+++ sentence-world-calculation.py
@@ -0,0 +1,7 @@
+print("in the name of Allah")
+print("Welcom to the Calculation of sentence words tell me your sentence to Computing words")
+
+S = input("Enter yourSentence : ")
+count_of_words = len(S.split(" "))
+print("you're sentence spilted words number is : " , count_of_words)
+    
